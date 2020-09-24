3bf25bc836c391ee6ee2cc08711ea56b3c8372eb
enhui.hu <huenhui@happycfc.com>
2020-09-24 17:16:17 +0800

    MySQL练习题

---
+++ docs/MySQL/README.md
@@ -0,0 +1,7 @@
+# MySQL
+
+[在线画图工具链接](https://www.processon.com/i/5e0d9502e4b02086237ce4f8)       
+[MySQL思维导图](https://www.processon.com/view/link/5f6c57d3e0b34d32792d4dde)      
+![Alt](http://assets.processon.com/chart_image/5f5a3e745653bb53ea9e74bc.png) 
+
+- [MySQL练习](MySQL/练习/)
---
+++ src/main/java/fun/enhui/thread/juc/apitest/AQSDemo.java
@@ -0,0 +1,24 @@
+package fun.enhui.thread.juc.apitest;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author 胡恩会
+ * @date 2020/8/21 16:40
+ */
+public class AQSDemo {
+
+
+    public static void main(String[] args) {
+        // 公平锁
+        Lock lock = new ReentrantLock(true);
+        try {
+            ThreadLocal threadLocal = new ThreadLocal();
+            lock.lock();
+        }finally {
+            lock.unlock();
+        }
+
+    }
+}
