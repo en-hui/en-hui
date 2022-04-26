da4099a39cd32e6545938f13033d84dacd581f5a
huenhui <717253212@qq.com>
2022-04-26 21:55:07 +0800

    消费最新100条消息

--- wealth-component-kafka/src/main/java/com/enhui/kafka/ConsumerTest.java
+++ wealth-component-kafka/src/main/java/com/enhui/kafka/ConsumerTest.java
@@ -3,12 +3,17 @@ package com.enhui.kafka;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.stream.Collectors;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -44,7 +49,7 @@ public class ConsumerTest {
     properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
     // kafka is MQ;is storage (存储)   所以要指定从哪里开始消费
 
-    properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+    properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
     // 自动提交（异步提交）；自动提交可能会重复消费||丢失消息
     properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
     // 默认5s自动提交
@@ -203,4 +208,93 @@ public class ConsumerTest {
               + record.value());
     }
   }
+
+  /** 从结尾倒数消费指定条数，特定的消费者组，无需提交 */
+  @Test
+  public void consumeByEndNoCommit() {
+    int count = 100;
+    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    // 不提交--关闭自动提交，且不手动提交
+    properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+    // 指定消费条数（如果有100条，就拉一百条，不够就算了）
+    properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, String.valueOf(count));
+    consumer = new KafkaConsumer<String, String>(properties);
+
+    try (KafkaConsumer<Object, Object> consumer = new KafkaConsumer<>(properties)) {
+      List<PartitionInfo> partitionInfos = consumer.partitionsFor(topic);
+      partitionInfos.stream().map(PartitionInfo::partition).forEach(System.out::println);
+      // 每个分区分别消费
+      List<ConsumerRecords<Object, Object>> multRecords = new ArrayList<>();
+      for (PartitionInfo partitionInfo : partitionInfos) {
+        TopicPartition topicPartition =
+                new TopicPartition(partitionInfo.topic(), partitionInfo.partition());
+        consumer.assign(Collections.singleton(topicPartition));
+        consumer.seekToEnd(Collections.singleton(topicPartition));
+        long position = consumer.position(topicPartition);
+        // position是下一次消息的偏移，而非最后一条的偏移，所以-1，减去数量需要+1才是第一个位置的offset，+-抵消
+        long currentMaxOffset = position - count > 0 ? position - count : 0;
+        consumer.seek(topicPartition, currentMaxOffset);
+        ConsumerRecords<Object, Object> records = consumer.poll(Duration.ofSeconds(10));
+        multRecords.add(records);
+      }
+      spliceMessageKV(count, multRecords);
+    }
+  }
+
+  @Test
+  public void test() {
+    List<Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    list.add(5);
+    list.add(3);
+    list.add(4);
+    list = list.stream().sorted(Comparator.comparing(Integer::intValue)).collect(Collectors.toList());
+    System.out.println(list);
+    int size = list.size();
+    int consumerCount = 2;
+    list = list.subList(size - consumerCount, size);
+    System.out.println(list);
+  }
+  private static void spliceMessageKV(
+    Integer consumerCount, Collection<ConsumerRecords<Object, Object>> mulRecords) {
+      List<ConsumerRecord<Object, Object>> allPartitionsRecords = new ArrayList<>();
+      for (ConsumerRecords<Object, Object> records : mulRecords) {
+        for (ConsumerRecord<Object, Object> record : records) {
+          allPartitionsRecords.add(record);
+        }
+      }
+      Integer finalCount = consumerCount;
+      if (mulRecords.size() > 1) {
+        allPartitionsRecords =
+                allPartitionsRecords.stream()
+                        .sorted(Comparator.comparing(ConsumerRecord::timestamp))
+                        .collect(Collectors.toList());
+        int size = allPartitionsRecords.size();
+        if (size > finalCount) {
+          allPartitionsRecords = allPartitionsRecords.subList(size - consumerCount, size - 1);
+        }
+      }
+      List<String> messages = new ArrayList<>(finalCount);
+    for (ConsumerRecord<Object, Object> record : allPartitionsRecords) {
+      long offset = record.offset();
+      StringBuilder sb = new StringBuilder(100);
+      sb.append("offset: ").append(offset).append("\n");
+      sb.append("partition: ").append(record.partition()).append("\n");
+      sb.append("timestamp: ").append(record.timestamp()).append("\n");
+      if (record.key() != null) {
+        sb.append("key: ").append(record.key().toString()).append("\n");
+      } else {
+        sb.append("key: ").append("{}").append("\n");
+      }
+      if (record.value() != null) {
+        sb.append("value: ").append(record.value().toString());
+      } else {
+        sb.append("value: ").append("{}");
+      }
+      sb.append("\n");
+      System.out.println(sb.toString());
+    }
+  }
+
 }
