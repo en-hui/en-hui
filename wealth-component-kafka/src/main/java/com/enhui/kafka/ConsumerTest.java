fc278edf3539c9a7fa3cc5a91360cda069843f17
huenhui <717253212@qq.com>
2022-05-03 17:55:33 +0800

    kafka 手动提交offset

--- wealth-component-kafka/src/main/java/com/enhui/kafka/ConsumerTest.java
+++ wealth-component-kafka/src/main/java/com/enhui/kafka/ConsumerTest.java
@@ -15,12 +15,13 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 import java.util.stream.Collectors;
-
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.clients.consumer.OffsetAndTimestamp;
 import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.TopicPartition;
@@ -45,45 +46,138 @@ public class ConsumerTest {
         ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
   }
 
-  /** 普通的正常消费 */
+  /**
+   * 消费者<br>
+   * 自动提交
+   */
   @Test
-  public void consumer() {
+  public void consumerWithAutoCommit() {
     // 消费者的细节
     properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
     // kafka is MQ;is storage (存储)   所以要指定从哪里开始消费
-
     properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
     // 自动提交（异步提交）；自动提交可能会重复消费||丢失消息
     properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
     // 默认5s自动提交
-    // properties.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG,"");
+    properties.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "10s");
     // POLL 拉取数据，弹性、按需，设置每次拉取多少（根据消费能力设定）
-    // properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,"");
+    properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
 
     consumer = new KafkaConsumer<String, String>(properties);
-    // 订阅 topic
     consumer.subscribe(Arrays.asList(topic));
-
     while (true) {
-      // 0~N
       ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(0));
-
       // 消费部分优化很重要
       Iterator<ConsumerRecord<String, String>> iterator = records.iterator();
       while (iterator.hasNext()) {
         // 一个consumer可以消费多个分区，但是一个分区只能给一个组里的一个consumer消费
         ConsumerRecord<String, String> record = iterator.next();
-        System.out.println(
-            "key:"
-                + record.key()
-                + " val:"
-                + record.value()
-                + " topic:"
-                + record.topic()
-                + " partition:"
-                + record.partition()
-                + " offset:"
-                + record.offset());
+        System.out.println("key: " + record.key() + "val: " + record.value());
+      }
+    }
+  }
+
+  /**
+   * 消费者<br>
+   * 手动提交——按记录提交offset
+   */
+  @Test
+  public void consumerWithCommitRecord() {
+    // 消费者的细节
+    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    // kafka is MQ;is storage (存储)   所以要指定从哪里开始消费
+    properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+    // POLL 拉取数据，弹性、按需，设置每次拉取多少（根据消费能力设定）
+    properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
+
+    consumer = new KafkaConsumer<String, String>(properties);
+    consumer.subscribe(Arrays.asList(topic));
+    while (true) {
+      ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(0));
+      // 消费部分优化很重要
+      Iterator<ConsumerRecord<String, String>> iterator = records.iterator();
+      while (iterator.hasNext()) {
+        // 一个consumer可以消费多个分区，但是一个分区只能给一个组里的一个consumer消费
+        ConsumerRecord<String, String> record = iterator.next();
+        System.out.println("key: " + record.key() + "val: " + record.value());
+        // 消费一条，提交一次offset
+        Map<TopicPartition, OffsetAndMetadata> map = new HashMap();
+        TopicPartition topicPartition = new TopicPartition(topic, record.partition());
+        OffsetAndMetadata offsetAndMetadata = new OffsetAndMetadata(record.offset());
+        map.put(topicPartition, offsetAndMetadata);
+        consumer.commitSync(map);
+      }
+    }
+  }
+
+  /**
+   * 消费者<br>
+   * 手动提交——按poll微批提交offset
+   */
+  @Test
+  public void consumerWithCommitPoll() {
+    // 消费者的细节
+    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    // kafka is MQ;is storage (存储)   所以要指定从哪里开始消费
+    properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+    // POLL 拉取数据，弹性、按需，设置每次拉取多少（根据消费能力设定）
+    properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
+
+    consumer = new KafkaConsumer<String, String>(properties);
+    consumer.subscribe(Arrays.asList(topic));
+    while (true) {
+      ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(0));
+      // 消费部分优化很重要
+      Iterator<ConsumerRecord<String, String>> iterator = records.iterator();
+      while (iterator.hasNext()) {
+        // 一个consumer可以消费多个分区，但是一个分区只能给一个组里的一个consumer消费
+        ConsumerRecord<String, String> record = iterator.next();
+        System.out.println("key: " + record.key() + "val: " + record.value());
+      }
+      // 消费poll批次的所有记录后，提交一次offset
+      consumer.commitSync();
+    }
+  }
+
+  /**
+   * 消费者<br>
+   * 手动提交——poll 回来的数据，按分区分别提交
+   */
+  @Test
+  public void consumerWithCommitPollPartition() {
+    // 消费者的细节
+    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    // kafka is MQ;is storage (存储)   所以要指定从哪里开始消费
+    properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+    // POLL 拉取数据，弹性、按需，设置每次拉取多少（根据消费能力设定）
+    properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
+
+    consumer = new KafkaConsumer<String, String>(properties);
+    consumer.subscribe(Arrays.asList(topic));
+    while (true) {
+      ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(0));
+      Set<TopicPartition> partitions = records.partitions();
+      for (TopicPartition partition : partitions) {
+        // 当前分区的所有记录（本次poll微批）
+        List<ConsumerRecord<String, String>> pRecords = records.records(partition);
+        // 先拿到当前分区的最后一条记录（提交offset时，需要知道首或者尾的offset）
+        ConsumerRecord<String, String> plastRecord = pRecords.get(pRecords.size() - 1);
+        Iterator<ConsumerRecord<String, String>> iterator = pRecords.iterator();
+        // 同一分区内数据，可以使用多线程处理（根据场景优化），只要最后提交事务和提交offset放在一个线程，控制提交首或尾就可以
+        while (iterator.hasNext()) {
+          // 一个consumer可以消费多个分区，但是一个分区只能给一个组里的一个consumer消费
+          ConsumerRecord<String, String> record = iterator.next();
+          System.out.println("key: " + record.key() + "val: " + record.value());
+        }
+        // poll中根据分区处理，分区分别提交
+        Map<TopicPartition, OffsetAndMetadata> map = new HashMap();
+        TopicPartition topicPartition = new TopicPartition(topic, plastRecord.partition());
+        OffsetAndMetadata offsetAndMetadata = new OffsetAndMetadata(plastRecord.offset());
+        map.put(topicPartition, offsetAndMetadata);
+        consumer.commitSync(map);
       }
     }
   }
@@ -229,7 +323,7 @@ public class ConsumerTest {
       List<ConsumerRecords<Object, Object>> multRecords = new ArrayList<>();
       for (PartitionInfo partitionInfo : partitionInfos) {
         TopicPartition topicPartition =
-                new TopicPartition(partitionInfo.topic(), partitionInfo.partition());
+            new TopicPartition(partitionInfo.topic(), partitionInfo.partition());
         consumer.assign(Collections.singleton(topicPartition));
         consumer.seekToEnd(Collections.singleton(topicPartition));
         long position = consumer.position(topicPartition);
@@ -244,56 +338,41 @@ public class ConsumerTest {
     }
   }
 
-  @Test
-  public void test() {
-    List<Integer> list = new ArrayList<>();
-    list.add(1);
-    list.add(2);
-    list.add(5);
-    list.add(3);
-    list.add(4);
-    list = list.stream().sorted(Comparator.comparing(Integer::intValue)).collect(Collectors.toList());
-    System.out.println(list);
-    int size = list.size();
-    int consumerCount = 2;
-    list = list.subList(size - consumerCount, size);
-    System.out.println(list);
-  }
   private static void spliceMessageKV(
-    Integer consumerCount, Collection<ConsumerRecords<Object, Object>> mulRecords) {
-      List<ConsumerRecord<Object, Object>> allPartitionsRecords = new ArrayList<>();
-      for (ConsumerRecords<Object, Object> records : mulRecords) {
-        for (ConsumerRecord<Object, Object> record : records) {
-          allPartitionsRecords.add(record);
-        }
+      Integer consumerCount, Collection<ConsumerRecords<Object, Object>> mulRecords) {
+    List<ConsumerRecord<Object, Object>> allPartitionsRecords = new ArrayList<>();
+    for (ConsumerRecords<Object, Object> records : mulRecords) {
+      for (ConsumerRecord<Object, Object> record : records) {
+        allPartitionsRecords.add(record);
       }
-      Integer finalCount = consumerCount;
-      if (mulRecords.size() > 1) {
-        allPartitionsRecords =
-                allPartitionsRecords.stream()
-                        .sorted(Comparator.comparing(ConsumerRecord::timestamp))
-                        .collect(Collectors.toList());
-        int size = allPartitionsRecords.size();
-        if (size > finalCount) {
-          allPartitionsRecords = allPartitionsRecords.subList(size - consumerCount, size);
-        }
+    }
+    Integer finalCount = consumerCount;
+    if (mulRecords.size() > 1) {
+      allPartitionsRecords =
+          allPartitionsRecords.stream()
+              .sorted(Comparator.comparing(ConsumerRecord::timestamp))
+              .collect(Collectors.toList());
+      int size = allPartitionsRecords.size();
+      if (size > finalCount) {
+        allPartitionsRecords = allPartitionsRecords.subList(size - consumerCount, size);
       }
+    }
     for (ConsumerRecord<Object, Object> record : allPartitionsRecords) {
       StringBuilder sb = new StringBuilder(100);
       long timestamp = record.timestamp();
       DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
       LocalDateTime localDateTime =
-              LocalDateTime.ofEpochSecond(timestamp / 1000, 0, ZoneOffset.ofHours(8));
+          LocalDateTime.ofEpochSecond(timestamp / 1000, 0, ZoneOffset.ofHours(8));
       String dateTime = localDateTime.format(formatter);
       sb.append("partition: ")
-              .append(record.partition())
-              .append("  ")
-              .append("offset: ")
-              .append(record.offset())
-              .append("  ")
-              .append("timestamp: ")
-              .append(dateTime)
-              .append("\n");
+          .append(record.partition())
+          .append("  ")
+          .append("offset: ")
+          .append(record.offset())
+          .append("  ")
+          .append("timestamp: ")
+          .append(dateTime)
+          .append("\n");
       if (record.key() != null) {
         sb.append("key: ").append(record.key().toString()).append("\n");
       } else {
@@ -308,5 +387,4 @@ public class ConsumerTest {
       System.out.println(sb.toString());
     }
   }
-
 }
