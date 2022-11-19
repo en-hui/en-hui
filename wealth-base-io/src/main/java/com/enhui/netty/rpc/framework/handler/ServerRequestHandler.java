5ebfe50bd06fc19acc4813b800cdf5137b6ff414
胡恩会 <enhuihu@qq.com>
2022-11-19 22:20:33 +0800

    netty rpc

--- wealth-base-io/src/main/java/com/enhui/netty/rpc/framework/handler/ServerRequestHandler.java
+++ wealth-base-io/src/main/java/com/enhui/netty/rpc/framework/handler/ServerRequestHandler.java
@@ -10,6 +10,7 @@ import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.concurrent.EventExecutor;
 
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectOutputStream;
@@ -18,14 +19,16 @@ public class ServerRequestHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         RpcMsgPackge packge = (RpcMsgPackge) msg;
-        System.out.printf("server read: %s\n", packge);
         String ioThreadName = Thread.currentThread().getName();
+        System.out.printf("%s:server read: %s\n",ioThreadName, packge);
         // 1、直接在当前方法处理
         // 2、使用netty当前的eventLoop来处理
-        ctx.executor().execute(()->{
+//        EventExecutor eventExecutor = ctx.executor();
         // 3、使用netty其他的eventLoop来处理
-        // todo，按理说应该用下面这种让线程忙碌程度均衡，但是使用下面这种，会出现问题：channelRead被调用30次，但下面这部分的打印只有 有限几次
-//        ctx.executor().parent().next().execute(() -> {
+        // todo，按理说应该用下面这种让线程忙碌程度均衡，
+        //  但是使用下面这种，会出现问题：在client是多个的时候，channelRead被调用30次，但下面这部分的打印只有 有限几次
+        EventExecutor eventExecutor = ctx.executor().parent().next();
+        eventExecutor.execute(() -> {
             try {
                 String execThreadName = Thread.currentThread().getName();
                 Object reqArg = ((RpcRequestContent) (packge.getContent())).getArgs()[0];
