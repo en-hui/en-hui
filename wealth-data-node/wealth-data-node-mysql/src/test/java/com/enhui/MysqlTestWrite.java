3525ce6813437499e6150d375a20a03a9551d1eb
huenhui <huenhui@datapipeline.com>
2025-02-12 15:58:45 +0800

    删除无用内容

--- wealth-data-node/wealth-data-node-mysql/src/test/java/com/enhui/MysqlTestWrite.java
+++
@@ -1,4 +0,0 @@
-package com.enhui;
-
-public class MysqlTestWrite {
-}
--- wealth-data-node/wealth-data-node-opengauss/src/main/java/com/enhui/Main.java
+++
@@ -1,7 +0,0 @@
-package com.enhui;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
